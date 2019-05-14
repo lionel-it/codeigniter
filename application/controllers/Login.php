c9b1595a8397a46b418698e8d8562adb1807fe21
Pham Ngoc Tuong <phamlionel1805@outlook.com>
2019-05-14 16:27:52 +0700

    Truyen Du Lieu Qua View

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -4,10 +4,16 @@ exit('No direct script access allowed');
 class Login extends CI_Controller
 {
     // Hàm load form login
-    public function load_form()
+    public function form()
     {
-        // Load view
-        $this->load->view('login_view');
+        // Data cần truyền qua view
+        $data = array(
+            'title' => 'Đây là trang login',
+            'message' => 'Nhập Thông Tin Đăng Nhập'
+        );
+  
+        // Load view và truyền data qua view
+        $this->load->view('login_view', $data);
     }
 }
 // Bạn ra trình duyệt gõ URL  http://localhost/ubuntu/codeIgniteronce/login/load_form
