1eccff8f58a0f37767d314fc9f9829bdafeb5fdb
Pham Ngoc Tuong <phamlionel1805@outlook.com>
2019-05-15 05:51:58 +0700

    Load library database 2

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -6,7 +6,12 @@ class User extends CI_Controller{
       $this->load->database();
   }
   public function index(){
-    $query = $this->db->query("SELECT * FROM user order By id");
+    // $query = $this->db->query("SELECT * FROM user order By id");
+    // $data = $query->result_array();
+    // echo "<pre>";
+    // print_r($data);
+    // echo "</pre>";
+    $query = $this->db->get("user");
     $data = $query->result_array();
     echo "<pre>";
     print_r($data);
