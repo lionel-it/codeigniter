fdf8cda095dedbb6bde783a9ef23c69c1d64aa40
Pham Ngoc Tuong <phamlionel1805@outlook.com>
2019-05-15 10:22:40 +0700

    resize & watermark

--- application/controllers/Upload.php
+++ application/controllers/Upload.php
@@ -17,29 +17,33 @@ class Upload extends CI_Controller{
             $config['max_height']  = '768';
             $this->load->library("upload", $config);
             if($this->upload->do_upload("img")){
-                echo 'Upload Ok';
-                    $check = $this->upload->data();
-                    echo "<pre>";
-                    print_r($check);
-                    echo "</pre>";
-                    $this->load->library("image_lib");
-                    /*$config['image_library'] = 'gd2';
-                    $config['source_image'] = './uploads/'.$check['file_name'];
-                    $config['create_thumb'] = TRUE;
-                    $config['maintain_ratio'] = TRUE;
-                    $config['width']     = 150;
-                    $config['height']   = 120;*/
- 
-                    $config['wm_text'] = 'Copyright 2014 - freetuts.net';
-                    $config['wm_type'] = 'text';
-                    $config['wm_font_path'] = './system/fonts/texb.ttf';
-                    $config['wm_font_size'] = '30';
-                    $config['wm_font_color'] = 'ffff00';
-                    $config['wm_vrt_alignment'] = 'bottom';
-                    $config['wm_hor_alignment'] = 'center';
-                    $config['wm_padding'] = '0';
-                    $this->image_lib->initialize($config);
-                    $this->image_lib->watermark();
+                echo "Upload thanh cong";
+                echo "<pre>";
+                print_r($this->upload->data());
+                echo "</pre>";
+                $check=$this->upload->data();
+                $this->load->library("image_lib");
+                $config['image_library'] = 'gd2';
+                $config['source_image'] = './uploads/'.$check['file_name'];
+                $config['create_thumb'] = TRUE;
+                $config['maintain_ratio'] = TRUE;
+                $config['width']     = 150;
+                $config['height']   = 120;
+                $this->image_lib->initialize($config);
+                $this->image_lib->resize();
+                $this->image_lib->clear();
+                unset($config);
+                
+                $config['source_image'] = './uploads/'.$check['file_name'];
+                $config['create_thumb'] = FALSE;
+                $config['wm_type'] = 'overlay';
+                $config['wm_overlay_path'] = './uploads/logo.png';
+                $config['wm_vrt_alignment'] = 'bottom';
+                $config['wm_hor_alignment'] = 'right';
+                $config['wm_padding'] = '0';
+                $config['wm_opacity'] = '50';
+                $this->image_lib->initialize($config);
+                $this->image_lib->watermark();
             }else{
                 $data['errors'] = $this->upload->display_errors();
                 $this->load->view("upload_view", $data);
