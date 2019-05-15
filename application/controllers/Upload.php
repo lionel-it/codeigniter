f974fa4693d360c7b4013ac7166d62653b234552
Pham Ngoc Tuong <phamlionel1805@outlook.com>
2019-05-15 10:21:04 +0700

    watermark image

--- application/controllers/Upload.php
+++ application/controllers/Upload.php
@@ -3,13 +3,11 @@ class Upload extends CI_Controller{
     public function __construct(){
         parent::__construct();
         $this->load->helper(array("form", "url"));
-    }
-      
+    } 
     public function index(){
         $data['errors'] = '';
         $this->load->view("upload_view", $data);
     }
-      
     public function doupload(){
         if($this->input->post("ok")){
             $config['upload_path'] = './uploads/';
@@ -24,19 +22,28 @@ class Upload extends CI_Controller{
                     echo "<pre>";
                     print_r($check);
                     echo "</pre>";
-                   $this->load->library("image_lib");
-                    $config['image_library'] = 'gd2';
+                    $this->load->library("image_lib");
+                    /*$config['image_library'] = 'gd2';
                     $config['source_image'] = './uploads/'.$check['file_name'];
                     $config['create_thumb'] = TRUE;
                     $config['maintain_ratio'] = TRUE;
                     $config['width']     = 150;
-                    $config['height']   = 120;
+                    $config['height']   = 120;*/
+ 
+                    $config['wm_text'] = 'Copyright 2014 - freetuts.net';
+                    $config['wm_type'] = 'text';
+                    $config['wm_font_path'] = './system/fonts/texb.ttf';
+                    $config['wm_font_size'] = '30';
+                    $config['wm_font_color'] = 'ffff00';
+                    $config['wm_vrt_alignment'] = 'bottom';
+                    $config['wm_hor_alignment'] = 'center';
+                    $config['wm_padding'] = '0';
                     $this->image_lib->initialize($config);
-                    $this->image_lib->resize();
+                    $this->image_lib->watermark();
             }else{
                 $data['errors'] = $this->upload->display_errors();
                 $this->load->view("upload_view", $data);
             }
         }
-    }       
+    }    
 }
