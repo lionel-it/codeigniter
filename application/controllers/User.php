2f51cc1c5465c58432247fbb3b9f861dbf76b246
Pham Ngoc Tuong <phamlionel1805@outlook.com>
2019-05-15 20:33:05 +0700

    CRUD Update

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -20,4 +20,48 @@ class User extends CI_Controller {
       $this->session->set_flashdata("flash_mess", "Delete Success");
       redirect(base_url() . "index.php/user");
   }
+  public function edit($id) {
+    $this->load->model('Muser');
+    $this->_data['titlePage'] = "Edit A User";
+    $this->_data['subview'] = "user/edit_view";
+    $this->_data['info'] = $this->Muser->getUserById($id);
+    $this->form_validation->set_rules("username", "Username", "required|min_length[4]|callback_check_user");
+    $this->form_validation->set_rules("password", "Password", "required");
+    $this->form_validation->set_rules("passwordcf", "Passwordcf", "matches[password]");
+    $this->form_validation->set_rules("email", "Email", "required|valid_email|callback_check_email");
+    if ($this->form_validation->run() == TRUE) {
+        $data_update = array(
+            "username" => $this->input->post("username"),
+            "email" => $this->input->post("email"),
+            "level" => $this->input->post("level"),
+        );
+        if ($this->input->post("password")) {
+            $data_update['password'] = $this->input->post("password");
+        }
+        $this->Muser->updateUser($data_update, $id);
+        $this->session->set_flashdata("flash_mess", "Update Success");
+        redirect(base_url() . "user");
+    }
+    $this->load->view('user/main.php', $this->_data);
+  }
+  public function check_user($user, $id) {
+    $this->load->model('Muser');
+    $id=$this->uri->segment(3);
+    if ($this->Muser->checkUsername($user, $id) == FALSE) {
+        $this->form_validation->set_message("check_user", "Your username has been registed, please try again!");
+        return FALSE;
+    } else {
+        return TRUE;
+    }
+  }
+  public function check_email($email,$id) {
+      $this->load->model('Muser');
+      $id=$this->uri->segment(3);
+      if ($this->Muser->checkEmail($email, $id) == FALSE) {
+          $this->form_validation->set_message("check_email", "Your email has been registed, please try again!");
+          return FALSE;
+      } else {
+          return TRUE;
+      }
+  }
 }
--- application/models/Muser.php
+++ application/models/Muser.php
@@ -15,4 +15,36 @@ class Muser extends CI_Model{
     $this->db->where('id',$id);
     $this->db->delete($this->_table);
   }
+  public function getUserById($id){
+    $this->db->where("id", $id);
+    return $this->db->get($this->_table)->row_array();
+  }
+  public function updateUser($data_update, $id){
+      $this->db->where("id", $id);
+      $this->db->update($this->_table, $data_update);
+  }
+  public function checkUsername($user, $id=""){
+    if($id != ""){
+      $this->db->where("id !=", $id);
+    }
+    $this->db->where('username',$user);
+    $query=$this->db->get($this->_table);
+    if($query->num_rows() > 0){
+      return FALSE;
+    }else{
+      return TRUE;
+    }
+  }
+  public function checkEmail($email,$id=""){
+    if($id != ""){
+      $this->db->where("id !=", $id);
+    }
+    $this->db->where('email',$email);
+    $query=$this->db->get($this->_table);
+    if($query->num_rows() > 0){
+      return FALSE;
+    }else{
+      return TRUE;
+    }
+  }
 }
