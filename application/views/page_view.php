369afc072a3fb360a37e0b3b96abe32ac6af0502
Pham Ngoc Tuong <phamlionel1805@outlook.com>
2019-05-15 08:43:16 +0700

    Url Helper

--- application/views/page_view.php
+++ application/views/page_view.php
@@ -2,4 +2,11 @@
 echo $this->pagination->create_links('Last');
 echo '<pre>';
 print_r($lionel);
+echo base_url();
+echo '<br/>';
+echo site_url();
+echo '<br/>';
+echo url_title('Hỗ trợ chuyển sang một trang được chỉ định.');
+echo '<br/>';
+echo anchor('news/local/123', 'My News', 'title="News title"');
 echo '<pre/>';
